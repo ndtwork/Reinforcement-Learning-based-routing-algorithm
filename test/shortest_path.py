31c77eda6d5fb6873eef5f6bcd8db273b9003936
tú <nguyenductu.work@gmail.com>
2024-12-22 20:21:19 +0700

    Update shortest_path.py

--- test/shortest_path.py
+++ test/shortest_path.py
@@ -63,75 +63,6 @@ def plot_graph(adjacency_matrix, figure_title=None, print_shortest_path=False, s
     img = img.reshape((h, w, 3))
     return img
 
-# def plot_graph(adjacency_matrix, figure_title=None, print_shortest_path=False, src_node=None, filename=None,
-#                added_edges=None, pause=False):
-#     adjacency_matrix = np.array(adjacency_matrix)
-#     rows, cols = np.where(adjacency_matrix > 0)
-#     edges = list(zip(rows.tolist(), cols.tolist()))
-#     values = [adjacency_matrix[i][j] for i, j in edges]
-#     weighted_edges = [(e[0], e[1], values[idx]) for idx, e in enumerate(edges)]
-#
-#     plt.cla()
-#
-#     # Create a new figure and axis object
-#     fig, ax = plt.subplots(figsize=(8, 6))  # You can adjust the figure size if needed
-#
-#     # Title setup
-#     if figure_title is None:
-#         ax.set_title("The shortest path for every node to the target")
-#     else:
-#         ax.set_title(figure_title)
-#
-#     # Create graph
-#     G = nx.Graph()
-#     G.add_weighted_edges_from(weighted_edges)
-#
-#     # Plot the graph
-#     labels = nx.get_edge_attributes(G, 'weight')
-#     pos = nx.kamada_kawai_layout(G)
-#
-#     # Draw nodes and edges
-#     nx.draw(G, pos=pos, ax=ax, with_labels=True, font_size=15)
-#     nodes = nx.draw_networkx_nodes(G, pos, ax=ax, node_color="y", edgecolors='black')
-#
-#     # Highlight source node if provided
-#     if src_node is not None:
-#         nodes = nx.draw_networkx_nodes(G, pos, ax=ax, nodelist=[src_node], node_color="g", edgecolors='black')
-#
-#     # Draw edge labels
-#     nx.draw_networkx_edge_labels(G, pos=pos, edge_labels=labels, ax=ax, font_size=15)
-#
-#     # Print shortest path using Dijkstra
-#     if print_shortest_path:
-#         print("The shortest path (dijkstra) is shown below: ")
-#         added_edges = []
-#         num_nodes = G.number_of_nodes()  # Use the number of nodes from the graph
-#         for node in range(1, num_nodes):
-#             shortest_path = nx.dijkstra_path(G, node, 0)  # Shortest path from node to 0
-#             print(f"{'->'.join([str(v) for v in shortest_path])}: {nx.dijkstra_path_length(G, node, 0)}")
-#             added_edges += list(zip(shortest_path, shortest_path[1:]))
-#
-#     # Draw the added edges (if any)
-#     if added_edges is not None:
-#         nx.draw_networkx_edges(G, pos, ax=ax, edgelist=added_edges, edge_color='r', width=2)
-#
-#     # Save figure if filename is provided
-#     if filename is not None:
-#         plt.savefig(filename)
-#
-#     # Pause or show the plot
-#     if pause:
-#         plt.pause(0.3)
-#     else:
-#         plt.show()
-#
-#     # Return the image for video generation
-#     img = None
-#     img = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
-#     w, h = fig.canvas.get_width_height()
-#     img = img.reshape((h, w, 3))
-#     return img
-
 
 def get_best_actions(D, states):
     best_actions = []
@@ -146,6 +77,7 @@ def print_best_actions(best_actions):
     best_actions_info = [
         "{}->{}".format(item[0], item[1]) for item in best_actions]
     return ", ".join(best_actions_info)
+
 
 def value_iteration(threshold=0.001, gamma=1, visualize=True):
     print("-" * 20)
